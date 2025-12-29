26a7b11b15d9402ece46677277f8f97714a63fdf
Arvin Garcia <arvin@arvingarcia.com>
2025-12-29 20:22:22 +0800

    feat: complete remove duplicates from sorted array

---
+++ two-pointers/028-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,11 @@
+class Solution {
+  removeDuplicates(nums) {
+    let l = 1;
+    for (let r = 1; r < nums.length; r++) {
+      if (nums[r] !== nums[r - 1]) {
+        nums[l++] = nums[r];
+      }
+    }
+    return l;
+  }
+}
