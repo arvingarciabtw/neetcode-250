d2a1ebcdca661fbc7b69b88a10d13815d1b62bab
Arvin Garcia <arvin@arvingarcia.com>
2026-01-10 09:00:02 +0800

    feat: complete permutation in string

---
+++ sliding-window/040-permutation-in-string.js
@@ -0,0 +1,56 @@
+class Solution {
+  checkInclusion(s1, s2) {
+    for (let i = 0; i < s2.length; i++) {
+      const substring = s2.slice(i, i + s1.length);
+      if (substring.split("").sort().join("") === s1.split("").sort().join(""))
+        return true;
+    }
+    return false;
+  }
+
+  // == OPTIMAL SLIDING WINDOW ==
+  checkInclusion(s1, s2) {
+    if (s1.length > s2.length) {
+      return false;
+    }
+
+    let s1Count = new Array(26).fill(0);
+    let s2Count = new Array(26).fill(0);
+    for (let i = 0; i < s1.length; i++) {
+      s1Count[s1.charCodeAt(i) - 97]++;
+      s2Count[s2.charCodeAt(i) - 97]++;
+    }
+
+    let matches = 0;
+    for (let i = 0; i < 26; i++) {
+      if (s1Count[i] === s2Count[i]) {
+        matches++;
+      }
+    }
+
+    let l = 0;
+    for (let r = s1.length; r < s2.length; r++) {
+      if (matches === 26) {
+        return true;
+      }
+
+      let index = s2.charCodeAt(r) - 97;
+      s2Count[index]++;
+      if (s1Count[index] === s2Count[index]) {
+        matches++;
+      } else if (s1Count[index] + 1 === s2Count[index]) {
+        matches--;
+      }
+
+      index = s2.charCodeAt(l) - 97;
+      s2Count[index]--;
+      if (s1Count[index] === s2Count[index]) {
+        matches++;
+      } else if (s1Count[index] - 1 === s2Count[index]) {
+        matches--;
+      }
+      l++;
+    }
+    return matches === 26;
+  }
+}
