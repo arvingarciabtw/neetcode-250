09667d599fd22814570ff4f7b21b67ea7c7124bf
Arvin Garcia <arvin@arvingarcia.com>
2026-01-25 20:54:08 +0800

    feat: complete simplify path

---
+++ stack/055-simplify-path.js
@@ -0,0 +1,18 @@
+class Solution {
+  simplifyPath(path) {
+    const stack = [];
+    const paths = path.split("/");
+
+    for (const cur of paths) {
+      if (cur === "..") {
+        if (stack.length) {
+          stack.pop();
+        }
+      } else if (cur !== "" && cur !== ".") {
+        stack.push(cur);
+      }
+    }
+
+    return "/" + stack.join("/");
+  }
+}
