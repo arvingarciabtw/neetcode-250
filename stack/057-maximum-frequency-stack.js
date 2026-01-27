c4082b44b67eab6362275e99479b597f0d9ab022
Arvin Garcia <arvin@arvingarcia.com>
2026-01-27 10:14:57 +0800

    feat: complete maximum frequency stack

---
+++ stack/057-maximum-frequency-stack.js
@@ -0,0 +1,28 @@
+class FreqStack {
+  constructor() {
+    this.cnt = new Map();
+    this.stacks = new Map();
+    this.maxCnt = 0;
+  }
+
+  push(val) {
+    const valCnt = (this.cnt.get(val) || 0) + 1;
+    this.cnt.set(val, valCnt);
+    if (valCnt > this.maxCnt) {
+      this.maxCnt = valCnt;
+      if (!this.stacks.has(valCnt)) {
+        this.stacks.set(valCnt, []);
+      }
+    }
+    this.stacks.get(valCnt).push(val);
+  }
+
+  pop() {
+    const res = this.stacks.get(this.maxCnt).pop();
+    this.cnt.set(res, this.cnt.get(res) - 1);
+    if (this.stacks.get(this.maxCnt).length === 0) {
+      this.maxCnt--;
+    }
+    return res;
+  }
+}
