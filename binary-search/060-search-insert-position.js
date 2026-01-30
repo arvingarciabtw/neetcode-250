730b688c1c064f5cecd7c150e063f6ce780f4f86
Arvin Garcia <arvin@arvingarcia.com>
2026-01-30 10:40:41 +0800

    feat: complete search insert position

---
+++ binary-search/060-search-insert-position.js
@@ -0,0 +1,23 @@
+class Solution {
+  /*
+    == BINARY SEARCH ==
+    O(log n) time complexity
+    O(1) space complexity
+   */
+  searchInsert(nums, target) {
+    let l = 0,
+      r = nums.length - 1;
+    while (l <= r) {
+      const mid = Math.floor((l + r) / 2);
+      if (nums[mid] === target) {
+        return mid;
+      }
+      if (nums[mid] > target) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    }
+    return l;
+  }
+}
