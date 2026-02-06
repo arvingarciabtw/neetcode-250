5a7f6036f509aae627d1ffee2b998667e4e24439
Arvin Garcia <arvin@arvingarcia.com>
2026-02-06 11:30:47 +0800

    feat: complete search in rotated sorted array

---
+++ binary-search/067-search-in-rotated-sorted-array.js
@@ -0,0 +1,42 @@
+class Solution {
+  /*
+    == BRUTE FORCE ==
+    O(n) time complexity
+    O(1) space complexity
+   */
+  search(nums, target) {
+    return nums.indexOf(target);
+  }
+
+  /*
+    == BINARY SEARCH (ONE PASS) ==
+    O(log n) time complexity
+    O(1) space complexity
+   */
+  search(nums, target) {
+    let l = 0,
+      r = nums.length - 1;
+
+    while (l <= r) {
+      const mid = Math.floor((l + r) / 2);
+      if (target === nums[mid]) {
+        return mid;
+      }
+
+      if (nums[l] <= nums[mid]) {
+        if (target > nums[mid] || target < nums[l]) {
+          l = mid + 1;
+        } else {
+          r = mid - 1;
+        }
+      } else {
+        if (target < nums[mid] || target > nums[r]) {
+          r = mid - 1;
+        } else {
+          l = mid + 1;
+        }
+      }
+    }
+    return -1;
+  }
+}
