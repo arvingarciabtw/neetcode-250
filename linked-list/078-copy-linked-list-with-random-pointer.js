36afbd2dce5180a68fbb10ab5ec1c5270d395182
Arvin Garcia <arvin@arvingarcia.com>
2026-02-18 11:10:01 +0800

    feat: complete copy linked list with random pointer

---
+++ linked-list/078-copy-linked-list-with-random-pointer.js
@@ -0,0 +1,29 @@
+class Solution {
+  /*
+    == HASH MAP (ONE PASS) ==
+    O(n) time complexity
+    O(n) space complexity
+   */
+  copyRandomList(head) {
+    const oldToCopy = new Map();
+    oldToCopy.set(null, null);
+
+    let cur = head;
+    while (cur !== null) {
+      if (!oldToCopy.has(cur)) {
+        oldToCopy.set(cur, new Node(0));
+      }
+      oldToCopy.get(cur).val = cur.val;
+      if (!oldToCopy.has(cur.next)) {
+        oldToCopy.set(cur.next, new Node(0));
+      }
+      oldToCopy.get(cur).next = oldToCopy.get(cur.next);
+      if (!oldToCopy.has(cur.random)) {
+        oldToCopy.set(cur.random, new Node(0));
+      }
+      oldToCopy.get(cur).random = oldToCopy.get(cur.random);
+      cur = cur.next;
+    }
+    return oldToCopy.get(head);
+  }
+}
